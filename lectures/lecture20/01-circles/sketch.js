6d7f864121a7b2ca2b9147b1f8812baa817a6f64
Em <eparks1@unca.edu>
2023-04-03 10:39:03 -0400

    class

---
+++ lectures/lecture20/01-circles/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+    const canvasWidth = window.innerWidth;
+    const canvasHeight = window.innerHeight; 
+    createCanvas(canvasWidth, canvasHeight);
+
+    // fill('red');
+    noFill();
+    // circle(100, 200, 50);
+    // circle(100, 250, 50);
+    // circle(100, 300, 50);
+    // circle(100, 350, 50);
+    // circle(100, 400, 50);
+
+    let counter =0;
+    while (counter<8) {
+        circle (100,200 + counter * 50, 50);
+        counter +=1;
+    }
+
+    drawGrid(canvasWidth, canvasHeight);
+}
